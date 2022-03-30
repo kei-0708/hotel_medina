a5fb9881a36dad9eea0aad3034866ec1ae2650c9
kei-0708 <75625801+kei-0708@users.noreply.github.com>
2022-03-30 23:17:39 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  var topBtn = $('.pagetop');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  //ドロワーメニュー
+  $("#MenuButton").click(function () {
+    // $(".l-drawer-menu").toggleClass("is-show");
+    // $(".p-drawer-menu").toggleClass("is-show");
+    $(".js-drawer-open").toggleClass("open");
+    $(".drawer-menu").toggleClass("open");
+    $("html").toggleClass("is-fixed");
+
+  });
+
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+  // ハンバーガーメニュー開閉
+  $(function(){
+    $('.js-header-fixed__btn').on('click',function(){
+        $(this).toggleClass('open');
+        $('.header-spnav').toggleClass('active');
+    });
+  });
+
+  // おすすめプラン
+  $('.js-plan__lists').slick({
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 6500,
+    cssEase: "linear",
+    swipe: false,
+    dots: false,
+    arrows: false,
+    slidesToScroll: 1,
+  });
+
+});
+
+
+
