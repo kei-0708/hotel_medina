573e89b205b6a6d122b196838a8287625e40e596
kei-0708 <75625801+kei-0708@users.noreply.github.com>
2022-04-13 23:28:17 +0900

    04.13

---
+++ js/gsap.js
@@ -0,0 +1,33 @@
+
+var textWrap1 = document.querySelectorAll('.js-top__title1');
+var textWrap2 = document.querySelectorAll('.js-top__title2');
+  textWrap1.forEach((t) => (
+	t.innerHTML = t.textContent.replace(/\S/g, '<span>$&</span>')
+));
+  textWrap2.forEach((t) => (
+	t.innerHTML = t.textContent.replace(/\S/g, '<span>$&</span>')
+));
+
+gsap.fromTo('.js-top__title1 > span', {
+	opacity:0,
+	scale: 0,
+  rotateZ: '-180deg'
+},{
+	opacity:1,
+	scale:1,
+  rotateZ: '0',
+	stagger:0.1,
+  delay: 0.5
+});
+
+gsap.fromTo('.js-top__title2 > span', {
+	opacity:0,
+	scale: 0,
+  rotateZ: '-90deg'
+},{
+	opacity:1,
+	scale:1,
+  rotateZ: '0',
+	stagger:0.2,
+  delay: 1.0
+});
--- js/script.js
+++ js/script.js
@@ -1,70 +1,100 @@
-
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  var topBtn = $('.pagetop');
-  topBtn.hide();
+  // var topBtn = $('.pagetop');
+  // topBtn.hide();
 
   // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
-  });
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 70) {
+  //     // 指定px以上のスクロールでボタンを表示
+  //     topBtn.fadeIn();
+  //   } else {
+  //     // 画面が指定pxより上ならボタンを非表示
+  //     topBtn.fadeOut();
+  //   }
+  // });
 
   // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
+  // topBtn.click(function () {
+  //   $('body,html').animate({
+  //     scrollTop: 0
+  //   }, 300, 'swing');
+  //   return false;
+  // });
 
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
+  // $("#MenuButton").click(function () {
+  //   // $(".l-drawer-menu").toggleClass("is-show");
+  //   // $(".p-drawer-menu").toggleClass("is-show");
+  //   $(".js-drawer-open").toggleClass("open");
+  //   $(".drawer-menu").toggleClass("open");
+  //   $("html").toggleClass("is-fixed");
+  // });
 
 
 
   // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
+  // $(document).on('click', 'a[href*="#"]', function () {
+  //   let time = 400;
+  //   let header = $('header').innerHeight();
+  //   let target = $(this.hash);
+  //   if (!target.length) return;
+  //   let targetY = target.offset().top - header;
+  //   $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+  //   return false;
+  // });
 
   // ハンバーガーメニュー開閉
   $(function(){
-    $('.js-header-fixed__btn').on('click',function(){
+    $('.js-header-fixed__bar').on('click',function(){
         $(this).toggleClass('open');
         $('.header-spnav').toggleClass('active');
     });
   });
 
-  // おすすめプラン
+
+  // カーテンアニメーション
+  $(window).scroll(function () {
+
+    $('.js-features__image').each(function (){
+      var imageOffset = $(this).offset().top;
+      var scrollPoint = $(window).scrollTop();
+      var windowHeight = $(window).height();
+
+      if (scrollPoint > imageOffset - windowHeight) {
+        $(this).addClass('c-open');
+      } else {
+        $(this).removeClass('c-open');
+      }
+    });
+  });
+
+  // おすすめプランのスライダー
   $('.js-plan__lists').slick({
     autoplay: true,
     autoplaySpeed: 0,
-    speed: 6500,
-    cssEase: "linear",
-    swipe: false,
+    speed: 6000,
+    // cssEase: "linear",
+    swipe: true,
     dots: false,
     arrows: false,
     slidesToScroll: 1,
+    slidesToShow: 4,
+    responsive: [{
+      breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+      }
+    }
+    ]
+  });
+
+  // よくある質問のアコーディオン
+  $(function () {
+    $('.js-faq__question-wrap').click(function () {
+      $(this).next('div').slideToggle();
+      $(this).find(".faq__arrow").toggleClass('open');
+    });
   });
 
 });
